0ec957de12b460ad05fb8004d37c0150960dd6e4
Santosh Luitel <santoshluitel19@outlook.com>
2024-03-28 22:20:21 +0545

    Map practice

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Main {
+  public static void main(String[] args) {
+    Map<String,Integer> courseMap = new HashMap<>();
+    courseMap.put("Santosh",1);
+    courseMap.put("bikash",2);
+    courseMap.put("renu",3);
+    courseMap.put("prabesh",4);
+
+//    Iterator<Map.Entry<String, Integer>> iterator = courseMap.entrySet().iterator();
+//    while (iterator.hasNext()){
+//      Map.Entry<String,Integer> entry = iterator.next();
+//      System.out.println(entry.getKey()+" ==> "+entry.getValue());
+//    }
+    for (Map.Entry<String,Integer>entry: courseMap.entrySet()){
+      System.out.println(entry.getValue());
+      System.out.println(entry.getKey());
+    }
+  }
+}
